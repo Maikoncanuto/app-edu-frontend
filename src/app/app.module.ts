ffadce23f0164531b96026750c598be088052460
Maikon Canuto <maikoncanuto@gmail.com>
2018-12-06 23:46:38 -0200

    Desenvolvimento: Precisa ajustar o dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,23 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule, PreloadAllModules } from '@angular/router';
+import { SidebarLeftComponent } from './dashboard/sidebar-left/sidebar-left.component';
+import { SidebarMenuComponent } from './dashboard/sidebar-menu/sidebar-menu.component';
+import { MainComponent } from './dashboard/main/main.component';
+import { ContainerComponent } from './dashboard/container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    SidebarLeftComponent,
+    SidebarMenuComponent,
+    MainComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
-    
     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
   providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
